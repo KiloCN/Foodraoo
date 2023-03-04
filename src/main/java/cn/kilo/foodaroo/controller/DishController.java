a643105fda30ecd626ccdf8459975d263e08bcfa
KiloCN <kilo.cn.li@gmail.com>
2023-03-05 02:26:42 +0800

    Implement several functions of Dish page search

--- src/main/java/cn/kilo/foodaroo/controller/DishController.java
+++ src/main/java/cn/kilo/foodaroo/controller/DishController.java
@@ -3,15 +3,21 @@ package cn.kilo.foodaroo.controller;
 import cn.kilo.foodaroo.common.Result;
 import cn.kilo.foodaroo.common.ThreadLocalUserId;
 import cn.kilo.foodaroo.dto.DishDto;
+import cn.kilo.foodaroo.pojo.Category;
+import cn.kilo.foodaroo.pojo.Dish;
+import cn.kilo.foodaroo.service.CategoryService;
 import cn.kilo.foodaroo.service.DishFlavorService;
 import cn.kilo.foodaroo.service.DishService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * The DishController class is responsible for handling HTTP requests related to Dish.
  *
@@ -26,6 +32,9 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+    @Autowired
+    private CategoryService categoryService;
+
     /**
      * Saves a new dish along with its flavors.
      *
@@ -34,10 +43,51 @@ public class DishController {
      * @return the Result object indicating the success of the operation.
      */
     @PostMapping
-    public Result<String> saveDish(HttpServletRequest request, @RequestBody DishDto dishDto){
+    public Result<String> saveDish(HttpServletRequest request, @RequestBody DishDto dishDto) {
         Long createrId = (Long) request.getSession().getAttribute("employeeId");
         ThreadLocalUserId.setUserId(createrId);
         dishService.saveWithFlavor(dishDto);
         return Result.success("Save new dish successfully");
+    }
+
+
+    /**
+     * Retrieves a paged list of dishes based on the given page number, page size, and dish name.
+     * @param page the page number to retrieve.
+     * @param pageSize the number of dishes to retrieve per page.
+     * @param name the name of the dish to search for.
+     * @return the Result object containing the paged list of dishes and metadata.
+     */
+    @GetMapping("/page")
+    public Result<Page> getPage(int page, int pageSize, String name) {
+        Page<Dish> pageInfo = new Page<>(page, pageSize);
+        Page<DishDto> dishDtoPage = new Page<>();
+
+        LambdaQueryWrapper<Dish> queryWrapper = new LambdaQueryWrapper<>();
+
+        queryWrapper.like(name != null, Dish::getName, name);
+        queryWrapper.orderByDesc(Dish::getUpdateTime);
+
+
+        Page<Dish> dishPage = dishService.page(pageInfo, queryWrapper);
+
+        BeanUtils.copyProperties(pageInfo, dishDtoPage, "records");
+        List<Dish> records = pageInfo.getRecords();
+        List<DishDto> list = null;
+
+        list = records.stream().map((item) -> {
+            DishDto dishDto = new DishDto();
+            BeanUtils.copyProperties(item,dishDto);
+
+            Long categoryId = item.getCategoryId();
+            Category category = categoryService.getById(categoryId);
+            dishDto.setCategoryName(category.getName());
+
+            return dishDto;
+        }).collect(Collectors.toList());
+
+        dishDtoPage.setRecords(list);
+        return Result.success(dishDtoPage);
+
     }
 }
